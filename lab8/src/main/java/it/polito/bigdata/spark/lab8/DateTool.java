91103af9510d7a2cad17340abcc79cce92a80578
Franco Ruggeri <fruggeri@kth.se>
2021-08-26 12:25:31 +0200

    complete lab8

--- lab8/src/main/java/it/polito/bigdata/spark/lab8/DateTool.java
+++ lab8/src/main/java/it/polito/bigdata/spark/lab8/DateTool.java
@@ -1,4 +1,4 @@
-package it.polito.bigdata.spark.example;
+package it.polito.bigdata.spark.lab8;
 
 import java.sql.Timestamp;
 import java.text.ParseException;
@@ -7,111 +7,54 @@ import java.util.Calendar;
 import java.util.Date;
 
 public class DateTool {
-
-	public static String DayOfTheWeek(Timestamp date) {
-
+	
+	public static String dayOfTheWeek(Date date) {
 		String dayOfTheWeek;
-		Date d = new Date(date.getTime());
 		Calendar cal = Calendar.getInstance();
-
-		cal.setTime(d);
+		cal.setTime(date);
 
 		switch (cal.get(Calendar.DAY_OF_WEEK)) {
 		case Calendar.SUNDAY:
 			dayOfTheWeek = "Sun";
 			break;
-
 		case Calendar.MONDAY:
 			dayOfTheWeek = "Mon";
 			break;
-
 		case Calendar.TUESDAY:
 			dayOfTheWeek = "Tue";
 			break;
-
 		case Calendar.WEDNESDAY:
 			dayOfTheWeek = "Wed";
 			break;
-
 		case Calendar.THURSDAY:
 			dayOfTheWeek = "Thu";
 			break;
-
 		case Calendar.FRIDAY:
 			dayOfTheWeek = "Fri";
 			break;
-
 		case Calendar.SATURDAY:
 			dayOfTheWeek = "Sat";
 			break;
-
 		default:
-			dayOfTheWeek = "Sat";
-
+			throw new RuntimeException("Unexpected switch case for day of the week");
 		}
 
 		return dayOfTheWeek;
 	}
 
-	public static String DayOfTheWeek(String date) {
+	public static String dayOfTheWeek(Timestamp date) {
+		return dayOfTheWeek(new Date(date.getTime()));
+	}
 
-		String dayOfTheWeek;
+	public static String dayOfTheWeek(String date) throws ParseException {
 		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
-		Date d = new Date();
-		Calendar cal = Calendar.getInstance();
-
-		try {
-			d = format.parse(date);
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-
-		cal.setTime(d);
-
-		switch (cal.get(Calendar.DAY_OF_WEEK)) {
-		case Calendar.SUNDAY:
-			dayOfTheWeek = "Sun";
-			break;
-
-		case Calendar.MONDAY:
-			dayOfTheWeek = "Mon";
-			break;
-
-		case Calendar.TUESDAY:
-			dayOfTheWeek = "Tue";
-			break;
-
-		case Calendar.WEDNESDAY:
-			dayOfTheWeek = "Wed";
-			break;
-
-		case Calendar.THURSDAY:
-			dayOfTheWeek = "Thu";
-			break;
-
-		case Calendar.FRIDAY:
-			dayOfTheWeek = "Fri";
-			break;
-
-		case Calendar.SATURDAY:
-			dayOfTheWeek = "Sat";
-			break;
-
-		default:
-			dayOfTheWeek = "Sat";
-
-		}
-
-		return dayOfTheWeek;
+		return dayOfTheWeek(format.parse(date));
 	}
 
 	public static int hour(Timestamp date) {
-
 		Date d = new Date(date.getTime());
 		Calendar cal = Calendar.getInstance();
-
 		cal.setTime(d);
-
 		return cal.get(Calendar.HOUR_OF_DAY);
 	}
 
