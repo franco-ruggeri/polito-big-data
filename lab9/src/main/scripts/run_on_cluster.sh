9554ec7e5145b4ec1134d7df75db8fdf6b0dd21a
Franco Ruggeri <fruggeri@kth.se>
2021-08-28 12:18:52 +0200

    complete lab9

---
+++ lab9/src/main/scripts/run_on_cluster.sh
@@ -0,0 +1,14 @@
+# Check arguments
+if [ $# -lt 2 ]; then
+	echo "Usage: $0 algorithm feature1 [feature2 ...]"
+	exit 1
+fi
+
+# Remove output folder of the previous run
+hdfs dfs -rm -r results
+
+# Run application
+spark-submit --class it.polito.bigdata.spark.lab9.SparkDriver --deploy-mode client --master yarn Lab9-1.0.0.jar /data/students/bigdata-01QYD/Lab9/ReviewsNewfile.csv results $@
+
+# Retrieve result
+hdfs dfs -getmerge results result.txt
